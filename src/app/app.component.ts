9f69a913d16ca0a0e9c215f9ba2a6cfaa1dbb773
marcelo <mhosan@gmail.com>
2021-07-16 19:50:52 -0300

    armando un wfs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GetDatosWebService } from './servicios/get-datos-web.service';
 
+let miMapa: any;
 declare let L: any;
 declare let $: any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,10 +11,14 @@ declare let $: any;
 })
 export class AppComponent {
   title = 'mapa';
+  public layerWFS: any;
+  constructor(private servicioDatosWeb : GetDatosWebService){}
   ngOnInit() {
     this.iniciarMapa();
   }
-
+  //===================================================================
+  // Iniciar el mapa
+  //===================================================================
   iniciarMapa() {
     const googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
       maxZoom: 20,
@@ -21,12 +26,28 @@ export class AppComponent {
       detectRetina: true
     });
     const osm2 = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 20 });
+
     var miMapa = L.map('mapid', {
       center: [-34.921136, -57.954712],
       zoom: 8,
       zoomControl: false,
       maxZoom: 20
     }).addLayer(googleHybrid);
-
+  }
+  
+  //===================================================================
+  // el wfs del ign
+  //===================================================================
+  capaWFS() {
+    if (miMapa.hasLayer(this.layerWFS)) {
+      miMapa.removeLayer(this.layerWFS);
+    }
+    this.servicioDatosWeb.getWfsIgn()
+      .subscribe(respuestaJson => {
+        console.log('El ign respondió: ', respuestaJson);
+        //this.layerWFS = this.servicioWfsIgn.getWfs(respuestaJson, 'Partido');
+        miMapa.addLayer(this.layerWFS);
+        miMapa.fitBounds(this.layerWFS.getBounds());
+      });
   }
 }
