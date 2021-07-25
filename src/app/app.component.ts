85478faf0b195586bd9171227f7aebcdfa9d2708
marcelo <mhosan@gmail.com>
2021-07-25 12:57:02 -0300

    agregando el control de zoom y el de capas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { CapaArbaPartidosService } from './servicios/capa-arba-partidos.service'
 
 declare let L;
 let miMapa: any;
+let controlLayers;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'mapa';
   public layerWFSIgn: any;
   public layerWFSArba: any;
@@ -33,6 +34,40 @@ export class AppComponent implements OnInit{
       detectRetina: true
     });
     const osm2 = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 20 });
+    //-----------------------------------------------------------------
+    const osm1 = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    });
+
+    //-----------------------------------------------------------------
+    const openmap = L.tileLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}", {
+      attribution: 'terms and feedback'
+    });
+
+    //-----------------------------------------------------------------
+    const googleMaps = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      detectRetina: true
+    });
+
+    //-----------------------------------------------------------------
+    //let urlBing2 ="http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=0&amp;dir=dir_n&username=''";         
+    //let urlBing ="http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1";
+    //const bing = L.tileLayer(urlBing2);
+
+    //-----------------------------------------------------------------
+    const esriSat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      { maxZoom: 22 });
+
+    //-----------------------------------------------------------------
+    const esriTransportes = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer/tile/{z}/{y}/{x}');
+    const wmsLayer = L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
+      layers: 'TOPO-OSM-WMS'
+    });
 
     miMapa = L.map('mapid', {
       contextmenu: true,
@@ -61,7 +96,37 @@ export class AppComponent implements OnInit{
       zoomControl: false,
       maxZoom: 20
     }).addLayer(osm2);
-    
+    L.control.scale().addTo(miMapa);
+    L.control.zoom({
+      position: 'bottomright'
+    }).addTo(miMapa);
+    const baseMaps = {
+      /*"Google Hibrido": googleHibrido,
+      "Google Callejero": googleCallejero,
+      "Google Fisico": googleTerrain,
+      "Google Satelital": googleSatelite, */
+      "Open Street Map": osm2,
+      "Mapbox": osm1,
+      "Mundialis": wmsLayer,
+      "Google callejero ": googleMaps,
+      "Google hibrido": googleHybrid,
+      //"Bing": bing,
+      "ArcGis OnLine": openmap,
+      "Esri sat": esriSat,
+      "Esri transportes": esriTransportes
+    };
+    let overlayMaps = {
+      //"Capa Cursos": capaCursos
+      //"Industrias cuenca Riachuelo": layerJson,
+      //"Centros y Organizaciones Sociales": layerJsonCtos,
+      //"Centros educativos Pcia.Bs.As.":  layerEducacionJson,
+      //"Escuelas": layerEscuelasJson,
+      //"FFCC (Ign, cortesia @FerroviarioK)": layerFFCC
+      //"FFCC versi�n @FerroviarioK": layerFFCCFerroviariok,
+      //"FFCC marcadores": layerFFCCFerroviariokMarcadores
+      //"Centros educativos Pcia. Bs.As.": layerJsonEdu
+    };
+    controlLayers = L.control.layers(baseMaps, overlayMaps, { position: 'topleft' }).addTo(miMapa);
   }
   //===================================================================
   // 
