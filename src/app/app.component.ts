c27f3b35716805373e55e0ea1e5896e08fcedea7
marcelo <mhosan@gmail.com>
2021-07-26 19:30:09 -0300

    agregado de lat y lng del mouse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -120,6 +120,25 @@ export class AppComponent implements OnInit {
       maxZoom: 20
     }).addLayer(osm2);
     L.control.scale().addTo(miMapa);
+  //   L.control.coordinates({
+  //     position:"bottomleft", //optional default "bootomright"
+  //     decimals:2, //optional default 4
+  //     decimalSeperator:".", //optional default "."
+  //     labelTemplateLat:"Latitude: {y}", //optional default "Lat: {y}"
+  //     labelTemplateLng:"Longitude: {x}", //optional default "Lng: {x}"
+  //     enableUserInput:true, //optional default true
+  //     useDMS:false, //optional default false
+  //     useLatLngOrder: true, //ordering of labels, default false-> lng-lat
+  //     markerType: L.marker, //optional default L.marker
+  //     markerProps: {}, //optional default {},
+  //     labelFormatterLng : funtion(lng){return lng+" lng"}, //optional default none,
+  //     labelFormatterLat : funtion(lat){return lat+" lat"}, //optional default none
+  //     customLabelFcn: function(latLonObj, opts) { "Geohash: " + encodeGeoHash(latLonObj.lat, latLonObj.lng)} //optional default none
+  // }).addTo(map);
+    L.control.coordinates({
+      labelTemplateLat : "Latitud: {y}",
+      labelTemplateLng : "Longitud: {x}"
+    }).addTo(miMapa);
     L.control.zoom({
       position: 'bottomright'
     }).addTo(miMapa);
