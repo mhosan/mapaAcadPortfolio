9b437a966f285dfff98e864ad793c3bcf496bc41
Marcelo Hosan <mhosan@gmail.com>
2024-03-05 23:06:11 -0300

    agregando botones de help

--- src/app/components/cad/cad.component.ts
+++ src/app/components/cad/cad.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./cad.component.css']
 })
 export class CadComponent {
-
+  public showControlesGeograficos: boolean = false;
+  public showAreaPoligonos: boolean = false;
   
+  verControlesGeograficos(){
+    this.showControlesGeograficos = !this.showControlesGeograficos;
+  }
+
+  switchShowCardHelpAreaPoligonos(){
+    this.showAreaPoligonos = !this.showAreaPoligonos;
+  }
 }
