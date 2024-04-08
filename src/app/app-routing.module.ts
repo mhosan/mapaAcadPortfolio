57dc0d83affa4d7396af07b0807f22006abdcb5e
Marcelo Hosan <mhosan@gmail.com>
2024-04-08 20:51:01 -0300

    probando fragment en un componente nuevo, link-interno, limpio y funca bien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { MapaComponent } from './components/mapa/mapa.component';
 import { CadComponent } from './components/cad/cad.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component'
+import { LinkInternoComponent } from './components/link-interno/link-interno.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'mapa', component: MapaComponent },
   { path: 'cad', component: CadComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'linkinterno', component: LinkInternoComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
 
