6ff940651ea3564018a8720380ba00d9af843638
Marcelo Hosan <mhosan@gmail.com>
2024-04-10 21:11:18 -0300

    cambiando app-routing.module a standalone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MapaComponent } from './components/mapa/mapa.component';
 import { CadComponent } from './components/cad/cad.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component'
-import { LinkInternoComponent } from './components/link-interno/link-interno.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'mapa', component: MapaComponent },
   { path: 'cad', component: CadComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'linkinterno', component: LinkInternoComponent },
+  { path: 'portfolio', //load lazy
+    loadComponent: ()=> import('./components/portfolio/portfolio.component').then(
+      m => m.PortfolioComponent)
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
-
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: true,
-    anchorScrolling: 'enabled'
-  })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { MainComponent } from "./app/components/main/main.component";
+import { provideRouter } from '@angular/router';
+import { provideAnimations} from '@angular/platform-browser/animations';
+import { routes } from './app/app-routing.module';
 
 
-bootstrapApplication(MainComponent)
+bootstrapApplication(MainComponent, {
+  providers: [
+    provideAnimations(),
+    provideRouter(routes)
+  ]
+})
   .catch(err => console.error(err));
