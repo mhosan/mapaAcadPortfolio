4702b90c7d20cc5ba13e05987b51bef82958c274
Marcelo Hosan <mhosan@gmail.com>
2024-08-11 16:48:18 -0300

    cargando una imagen en el componente ia

--- src/app/components/ia/ia.component.ts
+++ src/app/components/ia/ia.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { RouterLink, RouterOutlet, RouterLinkActive} from '@angular/router';
+import { FormsModule } from '@angular/forms';  // <-- Importar FormsModule
 
 
 @Component({
@@ -9,11 +10,32 @@ import { RouterLink, RouterOutlet, RouterLinkActive} from '@angular/router';
   imports: [
     CommonModule,
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    FormsModule
   ],
   templateUrl: './ia.component.html',
   styleUrl: './ia.component.css'
 })
 export class IaComponent {
+  selectedImage: string | ArrayBuffer | null = null;
+  imageUrl: string = '';
 
+  // Método para manejar la selección de archivo
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.selectedImage = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  // Método para manejar la entrada de la URL de la imagen
+  onUrlInput(): void {
+    this.selectedImage = this.imageUrl;
+  }
 }
