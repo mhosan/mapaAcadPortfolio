70faeedee620830b46229db9f1203aed2906c46b
Marcelo Hosan <mhosan@gmail.com>
2024-08-12 10:01:25 -0300

    comenzó a funcionar cuando se selecciona la imagen del file system local

--- src/app/components/ia/ia.component.ts
+++ src/app/components/ia/ia.component.ts
@@ -25,23 +25,49 @@ export class IaComponent {
   generatedText: string = '';
 
   constructor(private imageToTextService: ImageToTextService, private http: HttpClient) { }
-
-  // Método para manejar la selección de archivo
+  
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
-    if (input.files && input.files[0]) {
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () =>{
+        this.selectedImage = reader.result;
+      }
+      reader.readAsDataURL(file);// Esto ejecuta el reader y convierte la imagen a un string base64
+      //reader.readAsArrayBuffer(file);
+      // Convertir ArrayBuffer a Buffer
+      //const buffer = Buffer.from(this.arrayBufferMH);
+      //console.log('Buffer:', buffer);
+      const blob = new Blob([file], { type: file.type });   
+      this.imageToTextService.convertImageToText(blob).subscribe(response => {
+        this.generatedText = response[0]?.generated_text || 'No text generated';
+        console.log(`resultado: ${this.generatedText}`);
+      }); 
+    }
+  }
+
+  onUrlInput(): void {}
+
+  // Método para manejar la selección de archivo
+  /* onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
       const file = input.files[0];
       const reader = new FileReader();
       reader.onload = () => {
         this.selectedImage = reader.result;
+        console.log(`file: ${file}`);
+        console.log(`selectedImage es el resultado del reader.result: ${this.selectedImage}`);
+        const imageToStringBase64 = reader.readAsDataURL(file);// Esto convierte la imagen a un string base64
       };
-      reader.readAsDataURL(file);
       this.imageToTextService.convertImageToText(file).subscribe(response => {
         this.generatedText = response[0]?.generated_text || 'No text generated';
         console.log(`resultado: ${this.generatedText}`);
       });
     }
-  }
+  } */
+
   // Método para manejar la entrada de la URL de la imagen
   /* onUrlInput(): void {
     this.http.get(this.imageUrl, { responseType: 'blob' }).pipe(
@@ -59,12 +85,4 @@ export class IaComponent {
       }
     });
   } */
-    onUrlInput(): void {
-      this.imageToTextService.convertImageToText(this.imageUrl).subscribe(response => {
-        this.generatedText = response[0]?.generated_text || 'No text generated';
-        console.log(`resultado: ${this.generatedText}`);
-      }, err => {
-        console.error('Error al convertir la imagen a texto', err);
-      });
-    }
-  }
+}
--- src/app/servicios/image-to-text.service.ts
+++ src/app/servicios/image-to-text.service.ts
@@ -9,34 +9,17 @@ import { catchError } from 'rxjs/operators';
 export class ImageToTextService {
   private apiKey = "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";
   //private apiUrl = 'https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning';
-  private apiUrl = 'https://api-inference.huggingface.co/models/microsoft/git-base';
-  //private apiUrl = 'https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large';
+  //private apiUrl = 'https://api-inference.huggingface.co/models/microsoft/git-base';
+  private apiUrl = 'https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large';
   constructor(private http: HttpClient) { }
 
-  /*   convertImageToText(image: File): Observable<any> {
-      const headers = new HttpHeaders({
-        'Authorization': `Bearer ${this.apiKey}`,
-        'method': "POST",
-        'body': JSON.stringify(data),
-      });
-      const formData = new FormData();
-      formData.append('file', image);
-  
-      return this.http.post<any>(this.apiUrl, formData, { headers }).pipe(
-        catchError(err => {
-          console.error('Error al procesar la solicitud', err);
-          return throwError(err);
-        })
-      );
-    }
-   */
-  convertImageToText(data: any): Observable<any> {
+  convertImageToText(data: Blob): Observable<any> {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.apiKey}`,
       'Content-Type': 'application/json'
     });
 
-    return this.http.post<any>(this.apiUrl, JSON.stringify(data), { headers });
+    return this.http.post<any>(this.apiUrl, data, { headers });
 
   }
 }
