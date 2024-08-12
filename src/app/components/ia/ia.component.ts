166c87d4ad0193c049fe2e8232d62ea065f5cdd3
Marcelo Hosan <mhosan@gmail.com>
2024-08-12 10:35:12 -0300

    funcionando también el ingreso de la url de la imagen

--- src/app/components/ia/ia.component.ts
+++ src/app/components/ia/ia.component.ts
@@ -25,51 +25,26 @@ export class IaComponent {
   generatedText: string = '';
 
   constructor(private imageToTextService: ImageToTextService, private http: HttpClient) { }
-  
+
   onFileSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const file = input.files[0];
-      const reader = new FileReader();
-      reader.onload = () =>{
-        this.selectedImage = reader.result;
-      }
-      reader.readAsDataURL(file);// Esto ejecuta el reader y convierte la imagen a un string base64
-      //reader.readAsArrayBuffer(file);
-      // Convertir ArrayBuffer a Buffer
-      //const buffer = Buffer.from(this.arrayBufferMH);
-      //console.log('Buffer:', buffer);
-      const blob = new Blob([file], { type: file.type });   
-      this.imageToTextService.convertImageToText(blob).subscribe(response => {
-        this.generatedText = response[0]?.generated_text || 'No text generated';
-        console.log(`resultado: ${this.generatedText}`);
-      }); 
-    }
-  }
-
-  onUrlInput(): void {}
-
-  // Método para manejar la selección de archivo
-  /* onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
       const reader = new FileReader();
       reader.onload = () => {
         this.selectedImage = reader.result;
-        console.log(`file: ${file}`);
-        console.log(`selectedImage es el resultado del reader.result: ${this.selectedImage}`);
-        const imageToStringBase64 = reader.readAsDataURL(file);// Esto convierte la imagen a un string base64
-      };
-      this.imageToTextService.convertImageToText(file).subscribe(response => {
+      }
+      reader.readAsDataURL(file);// Esto ejecuta el reader y convierte la imagen a un string base64
+      const blob = new Blob([file], { type: file.type });
+      this.imageToTextService.convertImageToText(blob).subscribe(response => {
         this.generatedText = response[0]?.generated_text || 'No text generated';
         console.log(`resultado: ${this.generatedText}`);
       });
     }
-  } */
+  }
 
   // Método para manejar la entrada de la URL de la imagen
-  /* onUrlInput(): void {
+  onUrlInput(): void {
     this.http.get(this.imageUrl, { responseType: 'blob' }).pipe(
       catchError(err => {
         console.error('Error al obtener la imagen', err);
@@ -78,11 +53,14 @@ export class IaComponent {
     ).subscribe(blob => {
       if (blob) {
         const file = new File([blob], 'image.jpg');  // Nombre del archivo temporal
+        // Crear una URL a partir del Blob para mostrar la imagen
+        this.selectedImage = URL.createObjectURL(blob);
         this.imageToTextService.convertImageToText(file).subscribe(response => {
           this.generatedText = response[0]?.generated_text || 'No text generated';
           console.log(`resultado: ${this.generatedText}`);
         });
       }
     });
-  } */
+  }
+
 }
